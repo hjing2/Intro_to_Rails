1c8ed0aa1a295d5b6f447cde06eb14b3a4c53bb5
hjing2 <hjing2@academic.rrc.ca>
2024-06-28 01:04:56 -0500

    0628 added simple search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,21 +3,23 @@ class SearchController < ApplicationController
     @query = params[:q]
     @category = params[:category]
 
-    @results = if @category.present?
-        case @category
-        when 'addresses'
-          Address.where("street_name LIKE ?", "%#{@query}%")
-        when 'schools'
-          School.where("school LIKE ?", "%#{@query}%")
-        when 'parks'
-          Park.where("park_name LIKE ?", "%#{@query}%")
-        else
-          []
-        end
-      else
-        Address.where("street_name LIKE ?", "%#{@query}%")
-        .or(School.where("school LIKE ?", "%#{@query}%"))
-        .or(Park.where("park_name LIKE ?", "%#{@query}%"))
-      end
+    if @category.present?
+      @results = case @category
+                 when 'addresses'
+                   Address.where("street_name LIKE ?", "%#{@query}%")
+                 when 'schools'
+                   School.where("school LIKE ?", "%#{@query}%")
+                 when 'parks'
+                   Park.where("park_name LIKE ?", "%#{@query}%")
+                 else
+                   []
+                 end
+    else
+      address_results = Address.where("neighbourhood LIKE ?", "%#{@query}%")
+      school_results = School.where("street_name LIKE ?", "%#{@query}%")
+      park_results = Park.where("neighbourhood LIKE ?", "%#{@query}%")
+
+      @results = address_results + school_results + park_results
+    end
   end
 end
